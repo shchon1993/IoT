b542c1d7e5c5e3d0ca8b67307d2b8a00c161ca0b
gharlic <gharlic12@gmail.com>
2017-06-22 17:44:16 +0900

    Revert "cushion type"

--- test1.py
+++ test1.py
@@ -1,28 +1,20 @@
-
-mport RPi.GPIO as gpio
+import RPi.GPIO as gpio
 import time
 import datetime
  
 input_pin = 13
-
+ 
 input = 0
-count = 0
-
 gpio.setmode(gpio.BCM)
 gpio.setup(input_pin, gpio.IN)
  
 def input():
-    global input, count
+    global input
  
     if gpio.input(echo_pin) == 0:
-	count = count + 1
+        input = 0
     else input = 1
         pulse_end = time.time()
-	count = 0
-	
-    if count % 60 == 0:
-	input = 0
-
     if input == 1:
         print("1\n")
     else
@@ -34,5 +26,4 @@ try:
         time.sleep(1)
         
 except KeyboardInterrupt:
-gpio.cleanup()
-
+    gpio.cleanup()
